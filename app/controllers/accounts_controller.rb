cb7db8625559c122bf2c264f435f444e348de367
Ayla <aylanikkilorreen@gmail.com>
2019-11-25 22:42:03 +0800

    third commit

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,8 +3,16 @@ class AccountsController < ApplicationController
     before_action :set_account, only: [:profile]
 
     def index
-        @posts = Post.active
-        @follower_suggestions = Account.where.not(id: current_account.id)
+
+        followers_ids = Follower.where(follower_id: current_account.id).map(&:following_id)
+        followers_ids << current_account.id
+
+        @posts = Post.includes(:account).where(account_id: followers_ids).active
+
+        following_ids = Follower.where(follower_id: current_account.id).map(&:following_id)
+        following_ids << current_account.id
+
+        @follower_suggestions = Account.where.not(id: following_ids)
     end
 
     def profile
@@ -12,7 +20,14 @@ class AccountsController < ApplicationController
     end
 
     def follow_account
-        account_id = params[:follow_id]
+        follower_id = params[:follow_id]
+        if Follower.create!(follower_id: current_account.id, following_id: follower_id)
+            flash[:success] = "Now following user"
+        else
+            flash[:danger] = "Unable to follow user"
+        end
+        redirect_to dashboard_path
+
     end
 
     private
